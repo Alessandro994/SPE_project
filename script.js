8dd7a98e1c9c9e59c38938d4ebf42a92aba95cbc
Alessandro Bacchiega <bacchiega@fbk.eu>
2020-05-09 18:27:03 +0200

    reset node

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-import http from 'k6/http';
 import { sleep } from 'k6';
-
-import { exec } from 'child_process';
+import http from 'k6/http';
 
 // VU Init (this code is run only once)
-// const exec = require('child_process').exec, child;
-const myShellScript = exec('sh doSomething.sh /myDir');
 
 export default function () {
   http.get('http://localhost:8080');
-  // sleep(1);
+  sleep(1);
 }
