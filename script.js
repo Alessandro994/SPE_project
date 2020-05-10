3605d5a6b6eb6ff70abce40aa91c56b4276df929
Alessandro Bacchiega <bacchiega@fbk.eu>
2020-05-10 16:30:47 +0200

    remove npm

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-import { sleep } from 'k6';
 import http from 'k6/http';
 
 // VU Init (this code is run only once)
+// exec('ls');
 
 export default function () {
   http.get('http://localhost:8080');
-  sleep(1);
+  // sleep(1);
 }
