9554375b300df1bb7cb6eaecd2f014f50c744985
Alessandro Bacchiega <bacchiega@fbk.eu>
2020-05-11 16:01:41 +0200

    use custom port

--- server.ts
+++ server.ts
@@ -7,6 +7,8 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+const port = process.env.PORT
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
 });
