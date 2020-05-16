980a1d8348ace65cc69fee3d9362a5453391d571
Carlo Mion <mion00@gmail.com>
2020-05-16 18:00:33 +0200

    Add check for 200 status

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 import http from 'k6/http';
+import { check } from 'k6';
 
 // VU Init is run only once per VU.
 
 // VU code which is run over and over for as long as the test is running.
 export default function () {
-  http.get('http://localhost:8080');
+  let res = http.get('http://localhost:8080');
+
+  check(
+    res,
+    { "is status 200": r => r.status === 200 },
+    {
+      server_id: res.headers.server_id
+    }
+  );
+
   // sleep(1);
 }
