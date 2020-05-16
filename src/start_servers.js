de01b91a63865007796df9f6803d0a63ec632566
Carlo Mion <mion00@gmail.com>
2020-05-16 11:26:20 +0200

    start nginx with proxy servers

--- src/start_servers.js
+++ src/start_servers.js
@@ -9,14 +9,26 @@ if (!NUM_SERVERS) {
     throw new Exception("Please define env variable NUM_SERVERS")
 }
 
+function startNginx() {
+    console.info("Starting nginx on port 8080");
+    // Inherit stdout and stdin from parent process
+    const nginx = spawn('nginx', ["-c", "nginx.conf", "-p", process.cwd()], {stdio: 'inherit'});
+
+    // nginx.stdout.on('data', (data) => {
+    //     console.log(`${data}`);
+    // });
+
+    // nginx.stderr.on('data', (data) => {
+    //     console.log(`${data}`);
+    // });
+}
+
 let startingPort = 8081;
-
-
 for (let index = 0; index < NUM_SERVERS; index++) {
 
     // clone the actual env vars to avoid overrides
-    var env = Object.create( process.env );
-    env.PORT = startingPort+index;
+    var env = Object.create(process.env);
+    env.PORT = startingPort + index;
 
     const server = spawn('node', ["build/server.js"], { env: env });
 
@@ -32,3 +44,5 @@ for (let index = 0; index < NUM_SERVERS; index++) {
         console.log(`child process exited with code ${code}`);
     });
 }
+
+startNginx();
