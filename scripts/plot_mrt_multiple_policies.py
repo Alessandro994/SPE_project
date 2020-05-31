19833f878b61a1a2fe433377525c1b6f800f0866
Alessandro Bacchiega <bacchiega@fbk.eu>
2020-05-31 18:49:46 +0200

    style: 💄 add plots

--- scripts/plot_mrt_multiple_policies.py
+++ scripts/plot_mrt_multiple_policies.py
@@ -1,18 +1,21 @@
 # Plot the Averaged mean response time, confronting three Nginx policies in the same graph
-from steady_state_response_time import get_simulation_id, compute_mrt_for_simulation
 import matplotlib.pyplot as plt
 import numpy as np
+
+from steady_state_response_time import (compute_mrt_for_simulation,
+                                        get_simulation_id)
 
 if __name__ == "__main__":
     sim = get_simulation_id()
     print(f'Analyzing simulation {sim}')
 
-    policies = ['least-connected', 'random', 'round-robin']
+    policies = ['round-robin', 'random', 'least-connected']
     averages_response_time = []
     ci_intervals = []
 
     for i in range(0, 3):
-        response_time = compute_mrt_for_simulation(sim-i, autocorrelation_plot=False)
+        response_time = compute_mrt_for_simulation(
+            sim-i, autocorrelation_plot=False)
         averages_response_time.append(response_time.mean)
         ci_intervals.append(response_time.ci_interval)
 
