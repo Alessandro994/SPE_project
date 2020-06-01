10dc6d13fe4cec98de0a51693d724188abe01fac
Alessandro Bacchiega <bacchiega@fbk.eu>
2020-06-01 11:23:29 +0200

    fix: 🐛 config 3, 4\

--- runs/conf4.sh
+++ runs/conf4.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 export SEED=1000000000
+
+export NUM_SERVERS=1
+export MIN_RESPONSE_TIME=500
+export EXP_RESPONSE_TIME=true
+export K6_ITERATIONS=100000
+export K6_VUS=300
+export LAMBDA=0.01
+export K6_RPS=100
 export AUTOSCALE=true
+export SIMULATE_SERVER_LOAD=true
+
 export LOAD_BALANCING=least_conn
 echo "Starting $LOAD_BALANCING"
-NUM_SERVERS=1 MIN_RESPONSE_TIME=500 EXP_RESPONSE_TIME=true K6_ITERATIONS=100000 K6_VUS=300 LAMBDA=0.01 K6_RPS=100 AUTOSCALE=true npm run simulation
+npm run simulation
 sleep 5
 
 export LOAD_BALANCING=random
 echo "Starting $LOAD_BALANCING"
-NUM_SERVERS=1 MIN_RESPONSE_TIME=500 EXP_RESPONSE_TIME=true K6_ITERATIONS=100000 K6_VUS=300 LAMBDA=0.01 K6_RPS=100 AUTOSCALE=true npm run simulation
+npm run simulation
 sleep 5
 
 unset LOAD_BALANCING
 echo "Starting round-robin"
-NUM_SERVERS=1 MIN_RESPONSE_TIME=500 EXP_RESPONSE_TIME=true K6_ITERATIONS=100000 K6_VUS=300 LAMBDA=0.01 K6_RPS=100 AUTOSCALE=true npm run simulation
+npm run simulation
 sleep 5
