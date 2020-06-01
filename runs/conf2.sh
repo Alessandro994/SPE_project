4bc8d74ac8ed439c21d62b46ba82dd9bca751884
Alessandro Bacchiega <bacchiega@fbk.eu>
2020-06-01 12:20:36 +0200

    style: 💄 update plot 3

--- runs/conf2.sh
+++ runs/conf2.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 export SEED=1000000000
+
+export NUM_SERVERS=10
+export MIN_RESPONSE_TIME=500
+export EXP_RESPONSE_TIME=true
+export K6_ITERATIONS=100000
+export K6_VUS=300
+export LAMBDA=0.01
+export K6_RPS=100
+
 export LOAD_BALANCING=least_conn
 echo "Starting $LOAD_BALANCING"
-NUM_SERVERS=10 MIN_RESPONSE_TIME=500 EXP_RESPONSE_TIME=true K6_ITERATIONS=100000 K6_VUS=300 LAMBDA=0.01 K6_RPS=100 npm run simulation
+npm run simulation
 sleep 5
 
 export LOAD_BALANCING=random
 echo "Starting $LOAD_BALANCING"
-NUM_SERVERS=10 MIN_RESPONSE_TIME=500 EXP_RESPONSE_TIME=true K6_ITERATIONS=100000 K6_VUS=300 LAMBDA=0.01 K6_RPS=100 npm run simulation
+npm run simulation
 sleep 5
 
 unset LOAD_BALANCING
 echo "Starting round-robin"
-NUM_SERVERS=10 MIN_RESPONSE_TIME=500 EXP_RESPONSE_TIME=true K6_ITERATIONS=100000 K6_VUS=300 LAMBDA=0.01 K6_RPS=100 npm run simulation
+npm run simulation
 sleep 5
